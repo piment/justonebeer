d674e392ba8acca6244bfb4b9b4dfda1329687e4
Lucas-jimenez-dev <pro.lucasjimenez@gmail.com#Downpower2302!Lucas-jimenez-devgit config --global user.email pro.lucasjimenez@gmail.comgit config --global user.email pro.lucasjimenez@gmail.com#>
2022-04-02 14:45:13 +0200

    create function randomColumn and randomTime in utils.js

---
+++ src/lib/utils.js
@@ -0,0 +1,12 @@
+function randomColumn () {
+    let column = [80, 240, 400, 240, 400, 80, 400, 240, 80, 80, 400, 240];
+    return column[Math.floor(Math.random()*column.length)];
+}
+
+function randomTime (start, end) {
+    return Math.floor(Math.random(start, end) *(start - end) + end);
+}
+
+
+
+
